8ca1587caaa828f42591cd548791572a8c6e8abd
simituli <anupama.tuli@gmail.com>
2024-12-12 14:37:00 -0800

    Update sample.java Solution to https://leetcode.com/problems/merge-k-sorted-lists/

--- sample.java
+++ sample.java
@@ -1,5 +1,6 @@
 //Time Complexity: O(nLog(k)
 //Space Complexity: O(n)
+// Solution for https://leetcode.com/problems/kth-largest-element-in-an-array/
 class Solution {
     /* Min Heap
     public int findKthLargest(int[] nums, int k) {
@@ -27,5 +28,44 @@ class Solution {
                 result = Math.min(pq.poll(),result);
         }
         return result;
+    }
+}
+// Solution for https://leetcode.com/problems/merge-k-sorted-lists/
+TC:O(NLog(k) where k is the number of lists
+SC: O(N)
+/**
+ * Definition for singly-linked list.
+ * public class ListNode {
+ *     int val;
+ *     ListNode next;
+ *     ListNode() {}
+ *     ListNode(int val) { this.val = val; }
+ *     ListNode(int val, ListNode next) { this.val = val; this.next = next; }
+ * }
+ */
+class Solution {
+    public ListNode mergeKLists(ListNode[] lists) {
+        PriorityQueue<ListNode> pq = new PriorityQueue<>((a,b)->(a.val-b.val));
+        for(ListNode head: lists)
+        {
+            if(head!=null)
+            {
+                pq.add(head);
+            }
+        }
+        ListNode dummy = new ListNode(-1);
+        ListNode curr = dummy;
+        while(!pq.isEmpty())
+        {
+            ListNode minNode = pq.poll();
+            curr.next = minNode;
+            curr = curr.next;
+            if(minNode.next!=null)
+            {
+                pq.add(minNode.next);
+            }
+        }
+
+        return dummy.next;
     }
 }
